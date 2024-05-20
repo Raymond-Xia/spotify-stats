221bc47061ec202d52cba8e7f97a561fa0a3b828
Raymond Xia <ray.xia99@gmail.com>
2024-05-20 16:19:07 -0400

    Clean up

--- main/app.js
+++ main/app.js
@@ -1,12 +1,3 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
-
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var cors = require('cors');
@@ -15,7 +6,8 @@ var cookieParser = require('cookie-parser');
 
 var client_id = 'CLIENT_ID'; // Your client id
 var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var TOKEN = 'TOKEN'; // Your user-top-view token
 
 /**
  * Generates a random string containing numbers and letters
@@ -46,7 +38,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email user-top-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
@@ -59,7 +51,7 @@ app.get('/login', function(req, res) {
 
 app.get('/callback', function(req, res) {
 
-  // your application requests refresh and access tokens
+  // application requests refresh and access tokens
   // after checking the state parameter
 
   var code = req.query.code || null;
@@ -81,7 +73,7 @@ app.get('/callback', function(req, res) {
         grant_type: 'authorization_code'
       },
       headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
       },
       json: true
     };
@@ -89,12 +81,12 @@ app.get('/callback', function(req, res) {
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
 
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+        TOKEN = body.access_token,
+        refresh_token = body.refresh_token;
 
         var options = {
           url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
+          headers: { 'Authorization': 'Bearer ' + TOKEN },
           json: true
         };
 
@@ -106,7 +98,7 @@ app.get('/callback', function(req, res) {
         // we can also pass the token to the browser to make requests from there
         res.redirect('/#' +
           querystring.stringify({
-            access_token: access_token,
+            access_token: TOKEN,
             refresh_token: refresh_token
           }));
       } else {
@@ -125,7 +117,7 @@ app.get('/refresh_token', function(req, res) {
   var refresh_token = req.query.refresh_token;
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    headers: { 'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')) },
     form: {
       grant_type: 'refresh_token',
       refresh_token: refresh_token
@@ -141,6 +133,113 @@ app.get('/refresh_token', function(req, res) {
       });
     }
   });
+});
+
+app.get('/top/tracks/short', function(req, res) {
+  console.log(TOKEN);
+  var token = TOKEN;
+  authOptions = {
+    url: 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term',
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+  request.get(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var items = body.items;
+      res.send({
+        'items': items
+      });
+    }
+  });
+  
+});
+
+app.get('/top/tracks/medium', function(req, res) {
+  var token = TOKEN;
+  authOptions = {
+    url: 'https://api.spotify.com/v1/me/top/tracks?time_range=medium_term',
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+  request.get(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var items = body.items;
+      res.send({
+        'items': items
+      });
+    }
+  });
+});
+  
+app.get('/top/tracks/long', function(req, res) {
+  var token = TOKEN;
+  authOptions = {
+    url: 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term',
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+  request.get(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var items = body.items;
+      res.send({
+        'items': items
+      });
+    }
+  });
+});
+
+app.get('/top/artists/short', function(req, res) {
+  var token = TOKEN;
+  authOptions = {
+    url: 'https://api.spotify.com/v1/me/top/artists?time_range=short_term',
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+  request.get(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var items = body.items;
+      res.send({
+        'items': items
+      });
+    }
+  });
+  
+});
+
+app.get('/top/artists/medium', function(req, res) {
+  var token = TOKEN;
+  authOptions = {
+    url: 'https://api.spotify.com/v1/me/top/artists?time_range=medium_term',
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+  request.get(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var items = body.items;
+      res.send({
+        'items': items
+      });
+    }
+  });
+  
+});
+
+app.get('/top/artists/long', function(req, res) {
+  var token = TOKEN;
+  authOptions = {
+    url: 'https://api.spotify.com/v1/me/top/artists?time_range=long_term',
+    headers: { 'Authorization': 'Bearer ' + token },
+    json: true
+  };
+  request.get(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var items = body.items;
+      res.send({
+        'items': items
+      });
+    }
+  });
+  
 });
 
 console.log('Listening on 8888');
